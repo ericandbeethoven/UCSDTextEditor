58d20e2218ecd50b00f126478e805a8d01366034
ericandbeethoven <ebrucecfa@gmail.com>
2017-05-16 20:47:13 -0400

    Wk4 Markov Text Generation and Bug Fixes Commit

--- src/textgen/MarkovTextGenerator.java
+++ src/textgen/MarkovTextGenerator.java
@@ -11,7 +11,7 @@ public interface MarkovTextGenerator {
 	public void train(String sourceText);
 	
 	/** Generate the text with the specified number of words */
-	public String generateText(int numWords);
+	public String generateText(int numWords); 
 	
 	/** Retrain the generator from scratch on the source text */
 	public void retrain(String sourceText);
